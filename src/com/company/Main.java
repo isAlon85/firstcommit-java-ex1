1a44a1e6ca5be46a482293800c7c1c077fc6cf06
Israel Alonso <93823993+isAlon85@users.noreply.github.com>
2021-12-19 23:04:28 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,83 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static final String SEPARATOR = ";";
+
+    public static void main(String[] args) {
+        ArrayList<User> usersSaved;
+        usersSaved = addUsersFromCSV("nombres.csv");
+        System.out.println(usersSaved);
+    }
+
+    public static ArrayList<User> addUsersFromCSV(String file){
+        ArrayList<User> users = new ArrayList<>();
+        int actualLine = 1;
+
+        BufferedReader readBuffer = null;
+        try {
+            // Abrir el .csv en buffer de lectura
+            readBuffer = new BufferedReader(new FileReader(file));
+
+            // Leer una línea del archivo
+            String line = readBuffer.readLine();
+
+            while (line != null) {
+                // Separar la línea leída con el separador definido previamente
+                String[] fields = line.split(SEPARATOR);
+                User user = new User(fields[0], fields[1], fields[2]);
+                // Filtar emails duplicados y lineas con campos en blanco
+                boolean duplicatedMail = false;
+                for (User value : users) {
+                    if (Objects.equals(value.getEmail(), user.getEmail())) {
+                        duplicatedMail = true;
+                        break;
+                    }
+                }
+                if (fields[0].isBlank() || fields[1].isBlank() || fields[2].isBlank()) {
+                    System.err.println("Line " + actualLine + " is wrong");
+                } else if (!validateMail(fields[0])) {
+                    System.err.println("email " + fields[0] + " in line " + actualLine + " is not a proper email");
+                } else if (duplicatedMail){
+                    System.err.println("email " + fields[0] + " in line " + actualLine + " is duplicated");
+                }else {
+                    users.add(user);
+                }
+
+                // Volver a leer otra línea del fichero
+                line = readBuffer.readLine();
+                actualLine++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // Cierro el buffer de lectura
+            if (readBuffer != null) {
+                try {
+                    readBuffer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return users;
+    }
+
+    public static boolean validateMail(String email) {
+        Pattern pattern = Pattern
+                .compile("^[_A-Za-z0-9-+]+(\\.[_A-Za-z0-9-]+)*@"
+                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+
+        Matcher mather = pattern.matcher(email);
+        return mather.find();
+    }
+
+}
---
+++ src/com/company/User.java
@@ -0,0 +1,55 @@
+package com.company;
+
+public class User {
+
+    private String email;
+    private String name;
+    private String username;
+
+    public User() {
+    }
+
+    public User(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+
+    public User(String email, String name, String username) {
+        this.email = email;
+        this.name = name;
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
